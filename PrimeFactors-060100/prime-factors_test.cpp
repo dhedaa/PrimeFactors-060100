716dbd7747276a05c2a75dafc0b4dc49421610e3
donghyuck <dheda359@gmail.com>
2025-07-02 10:48:21 +0900

    [feature] 66

--- PrimeFactors-060100/prime-factors_test.cpp
+++ PrimeFactors-060100/prime-factors_test.cpp
@@ -40,3 +40,8 @@ TEST_F(PrimeFixture, of9) {
 	expected = { 3, 3 };
 	EXPECT_EQ(expected, prime_factor.of(9));
 }
+
+TEST_F(PrimeFixture, of12) {
+	expected = { 2, 2, 3 };
+	EXPECT_EQ(expected, prime_factor.of(12));
+}
